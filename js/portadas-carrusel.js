cf5e87fb95494b341cec120c67aa2db20161843b
t4k020 <t4k020@gmail.com>
2025-06-26 19:40:48 -0300

    datos personales y js
    hice otro datos personales en donde se pueda modificar los datos y añadi el js que mandaron los profes

---
+++ js/portadas-carrusel.js
@@ -0,0 +1,19 @@
+// items es un array que contiene todos los 60 items, con sus correspondientes campos
+import items from "../data/items.json" with { type: 'json' };
+
+// Obtener número aleatorio entre 1 y 60
+function numeroAleatorio() {
+  return Math.floor(Math.random() * 60) + 1;
+}
+
+// Función para obtener una portada aleatoria
+function obtenerPortadaAleatoria(){
+  let portadaAleatoria;
+
+  //Acá deben agregar el código para obtener una portada aleatoria (portada de algún item),
+  //y guardarla en la variable portadaAleatoria
+
+  return portadaAleatoria;
+}
+
+// A continuación, deben agregar el código para "enlazar" 5 portadas aleatorias al carrusel
