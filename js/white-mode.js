77362ad4b5f6da32a1e5e1b7a07989769696ecbd
t4k020 <t4k020@gmail.com>
2025-07-07 18:16:18 -0300

    whitemode en todas las paginas

---
+++ js/white-mode.js
@@ -0,0 +1,32 @@
+    let darkMode = document.getElementById("darkMode")
+    let imagen = document.getElementById("imagen")
+    let datosPersonales = document.getElementById("cdp")
+    let login = document.getElementById("login")
+
+    darkMode.addEventListener("click", function () {
+
+        if (document.body.classList.contains("whiteMode")) {
+        imagen.src = "../assets/img/logo-en-negro.png";
+        } else {
+        imagen.src = "../assets/img/logo-en-blanco.png";
+        }
+
+        if (darkMode.classList.contains("fa-moon")) {
+        darkMode.classList.remove("fa-moon");
+        darkMode.classList.add("fa-sun");
+        } else {
+        darkMode.classList.remove("fa-sun");
+        darkMode.classList.add("fa-moon");
+        }
+
+        document.body.classList.toggle("whiteMode")
+        document.getElementById("encabezado-principal").classList.toggle("whiteMode");
+        if(datosPersonales){
+            datosPersonales.classList.toggle("whiteModes")
+        }
+        if(login){
+            login.classList.toggle("whiteModes")
+        }
+
+    });
+
