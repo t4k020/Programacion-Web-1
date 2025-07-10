59c6bc9b6990d69ea2211cbe2c76bc13b93d57ab
t4k020 <t4k020@gmail.com>
2025-07-10 16:22:21 -0300

    test5
    la posicion absoluta funciono pero ahora veo si solo con / funciona en github y local

--- js/rating.js
+++ js/rating.js
@@ -1,7 +1,7 @@
 export function estrellas(rating) {
   
   let estrellas = '';
-  const fullStar = '<img src="/Programacion-Web-1/assets/img/64px-Full_Star_Yellow.svg.png" alt="Estrella llena" class="estrella">';
+  const fullStar = '<img src="/assets/img/64px-Full_Star_Yellow.svg.png" alt="Estrella llena" class="estrella">';
   let emptyStar = '<img src="/Programacion-Web-1/assets/img/64px-Empty_Star.svg.png" alt="Estrella vacía" class="estrella">';
 
 
